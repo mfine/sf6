ccbe09d00c2fe476b04df68c8a659b5dd6916b8d
Mark Fine <mark.fine@gmail.com>
2013-03-08 20:31:35 -0800

    bring in a base class and DRY it up.

--- lib/sf6.rb
+++ lib/sf6.rb
@@ -3,69 +3,64 @@ module SF6
   class Unavailable < StandardError
   end
 
-  def self.stamp(data, attr, bucket)
-    timestamp = Time.now.to_i / bucket
-    data[:counts][attr] = 0 unless data[:timestamps][attr] == timestamp
-    timestamp
-  end
+  module Base
 
-  def self.increment(data, attr, timestamp)
-    data[:timestamps][attr] = timestamp
-    data[:counts][attr] += 1
-  end
-
-  def self.test(data, attr, value, &blk)
-    if value == 0 || data[:counts][attr] < value
-      yield
-    else
-      raise(Unavailable, attr)
+    def reset!(attr)
+      data[:timestamps][attr] = 0
+      data[:counts][attr] = 0
     end
-  end
 
-  def self.reset!(data, attr)
-    data[:timestamps][attr] = 0
-    data[:counts][attr] = 0
-  end
+    private
 
-  module Limiter
-
-    def self.data
+    def data
       @data ||= {:counts => Hash.new(0), :timestamps => Hash.new(0)}
     end
 
-    def self.check(attr, value, bucket, &blk)
-      timestamp = SF6.stamp(data, attr, bucket)
-      SF6.increment(data, attr, timestamp)
-      SF6.test(data, attr, value) do
-        yield
-      end
+    def stamp(attr, bucket)
+      timestamp = Time.now.to_i / bucket
+      data[:counts][attr] = 0 unless data[:timestamps][attr] == timestamp
+      timestamp
     end
 
-    def self.reset!(attr)
-      SF6.reset!(data, attr)
+    def increment(attr, timestamp)
+      data[:timestamps][attr] = timestamp
+      data[:counts][attr] += 1
+    end
+
+    def test(attr, value, &blk)
+      if value == 0 || data[:counts][attr] < value
+        yield
+      else
+        raise(Unavailable, attr)
+      end
+    end
+  end
+
+  module Limiter
+    extend Base
+
+    def self.check(attr, value, bucket, &blk)
+      timestamp = stamp(attr, bucket)
+      increment(attr, timestamp)
+      test(attr, value) do
+        yield
+      end
     end
   end
 
   module Breaker
-
-    def self.data
-      @data ||= {:counts => Hash.new(0), :timestamps => Hash.new(0)}
-    end
+    extend Base
 
     def self.check(attr, value, bucket, &blk)
-      timestamp = SF6.stamp(data, attr, bucket)
-      SF6.test(data, attr, value) do
+      timestamp = stamp(attr, bucket)
+      test(attr, value) do
         begin
           yield
         rescue => e
-          SF6.increment(data, attr, timestamp)
+          increment(attr, timestamp)
           raise
         end
       end
-    end
-
-    def self.reset!(attr)
-      SF6.reset!(data, attr)
     end
   end
 end
